a01f79396688cbd52e400a668140cb6c09cde4dc
LoganOn <logan15.krawczyk@gmail.com>
2019-03-19 23:49:52 +0100

    "URL builder"

--- src/main/java/controller/BorderPaneMainController.java
+++ src/main/java/controller/BorderPaneMainController.java
@@ -10,6 +10,7 @@ import javafx.scene.control.ChoiceBox;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.Period;
 
 public class BorderPaneMainController {
     private LocalDate localDate;
@@ -53,6 +54,48 @@ public class BorderPaneMainController {
 
     @FXML
     private void applyOnClick() {
+        String value1 = (String) currencyChoiceBox1.getValue();
+        String value2 = (String) currencyChoiceBox2.getValue();
+        String per = (String) periodChoiceBox.getValue();
+        String url;
+        switch (per) {
+            case "Tydzień":
+                localDate = LocalDate.now();
+                date = localDate.minus(Period.ofWeeks(1));
+                url = "http://api.nbp.pl/api/exchangerates/rates/A/" + value1 + "/" + date.toString() + "/" + localDate.toString() + "/";
+                System.out.println(url);
+                break;
+            case "2 Tygodnie":
+                localDate = LocalDate.now();
+                date = localDate.minus(Period.ofWeeks(2));
+                url = "http://api.nbp.pl/api/exchangerates/rates/A/" + value1 + "/" + date.toString() + "/" + localDate.toString() + "/";
+                System.out.println(url);
+                break;
+            case "Miesiąc":
+                localDate = LocalDate.now();
+                date = localDate.minus(Period.ofMonths(3));
+                url = "http://api.nbp.pl/api/exchangerates/rates/A/" + value1 + "/" + date.toString() + "/" + localDate.toString() + "/";
+                System.out.println(url);
+                break;
+            case "1/2 Roku":
+                localDate = LocalDate.now();
+                date = localDate.minus(Period.ofMonths(1));
+                url = "http://api.nbp.pl/api/exchangerates/rates/A/" + value1 + "/" + date.toString() + "/" + localDate.toString() + "/";
+                System.out.println(url);
+                break;
+            case "1/4 Roku":
+                localDate = LocalDate.now();
+                date = localDate.minus(Period.ofMonths(6));
+                url = "http://api.nbp.pl/api/exchangerates/rates/A/" + value1 + "/" + date.toString() + "/" + localDate.toString() + "/";
+                System.out.println(url);
+                break;
+            case "Rok":
+                localDate = LocalDate.now();
+                date = localDate.minus(Period.ofYears(1));
+                url = "http://api.nbp.pl/api/exchangerates/rates/A/" + value1 + "/" + date.toString() + "/" + localDate.toString() + "/";
+                System.out.println(url);
+                break;
+        }
 
     }
 }
